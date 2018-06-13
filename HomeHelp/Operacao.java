642ecf998025f324f753839db2307435ac391521
rfmoreira <37915965+rfmoreira@users.noreply.github.com>
2018-06-13 14:03:03 -0300

    Update Operacao.java

--- HomeHelp/Operacao.java
+++ HomeHelp/Operacao.java
@@ -3,13 +3,15 @@
  * and open the template in the editor.
  */
 package HomeHelp;
-
+import java.util.Scanner;
 /**
  *
  * @author rafael
  */
 public class Operacao {
     public static void main (String [] arg){
+        Scanner input = new Scanner (System.in);
+        
         
     }
 }
